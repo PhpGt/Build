44d3df2a42ed0a3871772040cd57464b56fb22ff
Greg Bowler <greg.bowler@g105b.com>
2017-12-10 19:57:11 +0000

    Check installed version string

--- src/Requirement.php
+++ src/Requirement.php
@@ -1,7 +1,17 @@
 <?php
 namespace Gt\Build;
 
+use Composer\Semver\Semver;
+
 class Requirement {
+	const VERSION_ARGUMENTS = [
+		"*" => "--version",
+	];
+
+	const VERSION_REGEXES = [
+		"*" => "/([\d\.]+)/",
+	];
+
 	protected $name;
 	protected $version;
 
@@ -10,7 +20,53 @@ class Requirement {
 		$this->version = $version;
 	}
 
+	public function __toString():string {
+		return implode(" ", [
+			$this->name,
+			$this->version,
+		]);
+	}
+
 	public function check():bool {
-		// TODO: Check if the requirement is installed on the developer's system.
+		$arg = $this->getArgumentToCheckVersionOfCommand();
+		$versionCommand = implode(" ", [
+			$this->name,
+			$arg,
+		]);
+
+		exec($versionCommand, $output, $return);
+
+		if($return !== 0) {
+			throw new VersionCheckException($this->name);
+		}
+
+		$outputString = implode(" ", $output);
+		$versionInstalled = $this->getVersionFromVersionString($outputString);
+
+		return $this->isVersionStringValid($versionInstalled);
+	}
+
+	protected function getArgumentToCheckVersionOfCommand():string {
+		if(array_key_exists($this->name, self::VERSION_ARGUMENTS)) {
+			return self::VERSION_ARGUMENTS[$this->name];
+		}
+
+		return self::VERSION_ARGUMENTS["*"];
+	}
+
+	protected function getVersionFromVersionString(string $string):string {
+		if(array_key_exists($this->name, self::VERSION_REGEXES)) {
+			$pattern = self::VERSION_REGEXES[$this->name];
+		}
+		else {
+			$pattern = self::VERSION_REGEXES["*"];
+		}
+
+		preg_match($pattern, $string, $matches);
+		return $matches[1];
+	}
+
+	protected function isVersionStringValid(string $versionInstalled):bool {
+		return Semver::satisfies($versionInstalled, $this->version);
 	}
 }
