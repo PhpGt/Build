c7e92c58f762c857480d26ec962eb5b29e2afd93
Greg Bowler <greg.bowler@g105b.com>
2017-12-10 19:56:04 +0000

    Run build loop if requirements check out

--- src/BuildRunner.php
+++ src/BuildRunner.php
@@ -2,9 +2,20 @@
 namespace Gt\Build;
 
 class BuildRunner {
-	public static function run(string $path):int {
+	public static function run(string $path, bool $continue = true):void {
 		require(__DIR__ . "/../vendor/autoload.php");
 		$build = new Build($path);
-		return $build->buildAll();
+		$build->check();
+
+		do {
+			$updates = $build->build();
+
+			foreach($updates as $update) {
+				echo date("Y-m-d H:i:s");
+				echo "\t";
+				echo "Updated: $update" . PHP_EOL;
+			}
+			usleep(100000);
+		} while($continue);
 	}
 }
