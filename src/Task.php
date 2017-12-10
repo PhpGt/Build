3cdb630ff497fe3d9510934bafd11d73e28e8ff5
Greg Bowler <greg.bowler@g105b.com>
2017-12-10 19:56:51 +0000

    Perform checking and building

--- src/Task.php
+++ src/Task.php
@@ -4,36 +4,76 @@ namespace Gt\Build;
 use Webmozart\Glob\Glob;
 
 class Task {
+	protected $absolutePath;
 	protected $pathMatch;
+
+	protected $name;
 	/** @var Requirement[] */
 	protected $requirements;
 	protected $execute;
 
 	protected $fileHashList = [];
 
-	public function __construct(string $pathMatch, $details) {
+	public function __construct(string $basePath, string $pathMatch, $details) {
 		$this->pathMatch = $pathMatch;
+		$this->absolutePath = $basePath . "/" . $this->pathMatch;
 		$this->setDetails($details);
 	}
 
-	public function build(string $basePath):void {
-		foreach($this->requirements as $requirement) {
-			$requirement->check();
-		}
+	public function __toString():string {
+		return $this->name ?? $this->execute->command;
+	}
 
-		$absolutePath = $basePath . "/" . $this->pathMatch;
-		foreach(Glob::glob($absolutePath) as $matchedPath) {
-			$this->fileHashList[$matchedPath] = md5_file($matchedPath);
+	public function check():void {
+		foreach($this->requirements as $requirement) {
+			if(!$requirement->check()) {
+				throw new UnsatisfiedRequirementVersion($requirement);
+			}
 		}
 	}
 
+	public function build():bool {
+		$changes = false;
+
+		foreach(Glob::glob($this->absolutePath) as $matchedPath) {
+			$md5 = md5_file($matchedPath);
+			$existingMd5 = $this->fileHashList[$matchedPath] ?? null;
+
+			if($md5 !== $existingMd5) {
+				$this->execute();
+				$changes = true;
+			}
+
+			$this->fileHashList[$matchedPath] = $md5;
+		}
+
+		return $changes;
+	}
+
 	protected function setDetails($details):void {
-		$this->execute = $details->execute ?? null;
+		$this->execute = $details->execute;
+		$this->name = $details->name ?? $details->execute->command;
 
 		if(isset($details->requires)) {
 			foreach($details->requires as $key => $value) {
-				$this->requirements []= new Requirement($key, $value);
+				$this->requirements []= new Requirement(
+					$key,
+					$value
+				);
 			}
+		}
+	}
+
+	protected function execute():void {
+		$fullCommand = implode(" ", [
+			$this->execute->command,
+			$this->execute->arguments,
+		]);
+
+		exec($fullCommand, $output, $return);
+
+		if($return !== 0) {
+			throw new TaskExecutionFailureException($fullCommand);
 		}
 	}
 }
