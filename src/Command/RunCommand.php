8ffedc19f6ba7c9a997d915a09acf162b7f99850
Greg Bowler <greg.bowler@g105b.com>
2019-03-14 16:09:19 +0000

    Tidy up run command with CLI functions

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -3,18 +3,45 @@ namespace Gt\Build\Command;
 
 use Gt\Cli\Argument\ArgumentValueList;
 use Gt\Cli\Command\Command;
+use Gt\Cli\Parameter\NamedParameter;
+use Gt\Cli\Parameter\Parameter;
 
 class RunCommand extends Command {
-	public function __construct() {
-		$this->setName("watch");
-		$this->setOptionalParameter(
-			false,
-			"watch",
-			"w"
-		);
-	}
-
 	public function run(ArgumentValueList $arguments = null):void {
 		$this->writeLine("Run command");
+	}
+
+	public function getName():string {
+		return "run";
+	}
+
+	public function getDescription():string {
+		return "Start the local webserver, crontab and client side build watcher.";
+	}
+
+	/** @return  NamedParameter[] */
+	public function getRequiredNamedParameterList():array {
+		return [];
+	}
+
+	/** @return  NamedParameter[] */
+	public function getOptionalNamedParameterList():array {
+		return [];
+	}
+
+	/** @return  Parameter[] */
+	public function getRequiredParameterList():array {
+		return [];
+	}
+
+	/** @return  Parameter[] */
+	public function getOptionalParameterList():array {
+		return [
+			new Parameter(
+				false,
+				"watch",
+				"w"
+			),
+		];
 	}
 }
